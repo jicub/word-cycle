43447157d91d717f40ce064709384232abb658cb
jicub <illo@jacobmallach.com>
2015-04-15 18:11:49 -0700

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,66 @@
+module.exports = function(grunt) {
+  // Do grunt-related things in here
+
+  // Load npm tasks
+  require('load-grunt-tasks')(grunt);
+
+	// configure the tasks
+	grunt.initConfig({
+		// This line makes your node configurations available for use
+		pkg: grunt.file.readJSON('package.json'),
+		compass: {                  // Task
+			dist: {                   // Target
+				options: {
+					config: 'config.rb',
+					sassDir: 'sass',
+					cssDir: 'css',
+					environment: 'production'
+				}
+			}
+		},
+		connect: {
+			server: {
+				options: {
+					base: '.',
+					port: 9000,
+					hostname: '0.0.0.0'
+				}
+			}
+		},
+		watch: {
+			options: {
+				livereload: 1337
+			},
+			css: {
+				// Watch all Sass files
+				files: ['sass/**/*.{sass,scss}'],
+				tasks: ['compass']
+			},
+			js: {
+				files: ['js/**/*.js'],
+				tasks: ['jshint']
+			},
+			image: {
+				cwd: 'img/min/',
+				files: ['**/*.{png, jpg, gif}'],
+			}
+		},
+		jshint: {
+			all: [
+				'js/**/*.js',
+				'!js/classListShim.js',
+				'!js/libs/**/*.js',
+				'!js/plugins/**/*.js',
+				'!js/app.config.js'
+			]
+		}
+	});
+
+	//Register Tasks
+	//Launch Grunt Server
+	grunt.registerTask('server', [
+		'jshint',
+		'connect',
+		'watch'
+	]);
+};
---
+++ README.md
@@ -0,0 +1,11 @@
+Vanilla JS with CSS3 Animation
+===================
+
+### Decription
+
+This demo dynamically cycles through an array of words. Then animates each element into view with the help of some vanilla javascript and CSS3.
+
+### Demo
+http://jicub.github.io/git-demo-word-cycle-js
+
+
